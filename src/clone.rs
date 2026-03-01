c84e1e8c22fab36a87c7f3d9552ddd1f5c9757b1
Alexandru Agape <mail@alexagape.com>
2026-03-01 18:07:41 +0100

    Set upstream for all local branches

--- src/clone.rs
+++ src/clone.rs
@@ -1,3 +1,4 @@
+use git2::FetchOptions;
 use git2::{Cred, RemoteCallbacks, Repository, build::RepoBuilder};
 use ssh_key::PrivateKey;
 use std::io::{Read, Write};
@@ -21,7 +22,7 @@ fn read_file(path: &PathBuf) -> Result<String, String> {
     return Err(format!("❌ Could not open file at {:?}", path));
 }
 
-fn get_ssh_keys(path: PathBuf) -> Vec<PathBuf> {
+fn get_ssh_keys(path: &PathBuf) -> Vec<PathBuf> {
     let mut ret: Vec<PathBuf> = Vec::new();
     if path.is_file() {
         let encoded_key = read_file(&path).unwrap();
@@ -30,7 +31,7 @@ fn get_ssh_keys(path: PathBuf) -> Vec<PathBuf> {
             panic!("❌ Could not identify a valid SSH key in {:?}", path);
         });
 
-        ret.push(path);
+        ret.push(path.clone());
     } else if path.is_dir() {
         let files = fs::read_dir(&path).unwrap_or_else(|_| {
             panic!("❌ Directory '{:?}' is empty", path);
@@ -38,7 +39,6 @@ fn get_ssh_keys(path: PathBuf) -> Vec<PathBuf> {
 
         for file in files {
             if let Ok(_file) = file {
-                println!("{:?}", _file.path());
                 let encoded_key = read_file(&_file.path()).unwrap();
                 if let Ok(_) = PrivateKey::from_openssh(encoded_key) {
                     ret.push(_file.path());
@@ -48,6 +48,65 @@ fn get_ssh_keys(path: PathBuf) -> Vec<PathBuf> {
         assert!(ret.len() > 0);
     }
     return ret;
+}
+
+fn get_credential_callbacks(ssh_key: &PathBuf) -> RemoteCallbacks<'_> {
+    let ssh_keys = get_ssh_keys(&ssh_key);
+    let mut key_iter = ssh_keys.into_iter();
+    let mut callbacks = RemoteCallbacks::new();
+
+    callbacks.credentials(move |_url, username_from_url, allowed_types| {
+        let username =
+            username_from_url.ok_or_else(|| git2::Error::from_str("No username provided"))?;
+
+        // Only try ssh keys if ssh is allowed
+        if allowed_types.is_ssh_key() {
+            if let Some(key_path) = key_iter.next() {
+                return Cred::ssh_key(username, None, &key_path, None);
+            }
+        }
+
+        Err(git2::Error::from_str("No more SSH keys to try"))
+    });
+
+    return callbacks;
+}
+
+pub fn set_upstream_for_branches(repo: &mut Repository, ssh_key: &PathBuf) -> Result<(), String> {
+    repo.remote_add_fetch("origin", "+refs/heads/*:refs/remotes/origin/*")
+        .unwrap();
+
+    let callbacks = get_credential_callbacks(&ssh_key);
+
+    let mut fo = FetchOptions::new();
+    fo.remote_callbacks(callbacks);
+
+    let mut origin = repo.find_remote("origin").unwrap();
+    origin.fetch::<&str>(&[], Some(&mut fo), None).unwrap();
+
+    let branches = repo.branches(Some(git2::BranchType::Local)).unwrap();
+    for branch in branches {
+        let (mut branch, _) = branch.unwrap();
+        let branch_name = branch
+            .name()
+            .unwrap() // Result<Option<&str>, git2::Error>
+            .expect("Branch has no name")
+            .to_string();
+        if let Ok(remote_branch) =
+            repo.find_branch(&format!("origin/{}", branch_name), git2::BranchType::Remote)
+        {
+            let remote_name = remote_branch.name().unwrap();
+            match branch.set_upstream(remote_name) {
+                Ok(_) => continue,
+                Err(e) => panic!(
+                    "Could not set upstream for branch {}. Error: {}",
+                    &branch_name, e
+                ),
+            }
+        }
+    }
+
+    Ok(())
 }
 
 pub fn clone_repository(
@@ -75,25 +134,7 @@ pub fn clone_repository(
     if !repo_url.contains("https") {
         // SSH repo URL
         // Find possible ssh keys in $HOME/.ssh/
-        let ssh_keys = get_ssh_keys(ssh_key);
-        let mut key_iter = ssh_keys.into_iter();
-        let mut callbacks = RemoteCallbacks::new();
-
-        callbacks.credentials(move |_url, username_from_url, allowed_types| {
-            let username =
-                username_from_url.ok_or_else(|| git2::Error::from_str("No username provided"))?;
-
-            // Only try ssh keys if ssh is allowed
-            if allowed_types.is_ssh_key() {
-                if let Some(key_path) = key_iter.next() {
-                    println!("Trying SSH key: {:?}", key_path);
-                    return Cred::ssh_key(username, None, &key_path, None);
-                }
-            }
-
-            Err(git2::Error::from_str("No more SSH keys to try"))
-        });
-
+        let callbacks = get_credential_callbacks(&ssh_key);
         // Prepare fetch options.
         let mut fo = git2::FetchOptions::new();
         fo.remote_callbacks(callbacks);
@@ -102,7 +143,7 @@ pub fn clone_repository(
         repo_builder.fetch_options(fo);
     }
 
-    let _repo = repo_builder
+    let mut _repo = repo_builder
         .clone(&repo_url, &repo_path.join(".bare"))
         .expect("❌ Could not clone repository");
 
@@ -113,6 +154,9 @@ pub fn clone_repository(
 
     file.write_all(b"gitdir: ./.bare\n")
         .expect("❌ Could not write to the .git file.");
+
+    set_upstream_for_branches(&mut _repo, &ssh_key).unwrap();
+
     println!(
         "✅ Repository '{}' cloned successfully at '{}'",
         repo_url,
