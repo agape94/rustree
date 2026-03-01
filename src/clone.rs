82ac6eb531b1d07020b4042dfd86db4f30a4ec33
Alexandru Agape <mail@alexagape.com>
2026-03-01 14:32:36 +0100

    Add '❌' emoji in case of errors

--- src/clone.rs
+++ src/clone.rs
@@ -13,12 +13,12 @@ fn read_file(path: &PathBuf) -> Result<String, String> {
 
         let mut encoded_key = String::new();
         fs::File::read_to_string(&mut file, &mut encoded_key).unwrap_or_else(|_| {
-            panic!("Could not read file from '{:?}'", path);
+            panic!("❌ Could not read file from '{:?}'", path);
         });
 
         return Ok(encoded_key);
     }
-    return Err(format!("Could not open file at {:?}", path));
+    return Err(format!("❌ Could not open file at {:?}", path));
 }
 
 fn get_ssh_keys(path: PathBuf) -> Vec<PathBuf> {
@@ -27,13 +27,13 @@ fn get_ssh_keys(path: PathBuf) -> Vec<PathBuf> {
         let encoded_key = read_file(&path).unwrap();
 
         PrivateKey::from_openssh(encoded_key).unwrap_or_else(|_| {
-            panic!("Could not identify a valid SSH key in {:?}", path);
+            panic!("❌ Could not identify a valid SSH key in {:?}", path);
         });
 
         ret.push(path);
     } else if path.is_dir() {
         let files = fs::read_dir(&path).unwrap_or_else(|_| {
-            panic!("Directory '{:?}' is empty", path);
+            panic!("❌ Directory '{:?}' is empty", path);
         });
 
         for file in files {
@@ -67,7 +67,7 @@ pub fn clone_repository(
     match fs::create_dir_all(&repo_path) {
         Ok(_) => println!("Created repository directory {:?}", repo_path),
         Err(_) => {
-            println!("Repository already exists! Exiting...");
+            println!("❌ Repository already exists! Exiting...");
             exit(-1);
         }
     };
@@ -104,7 +104,7 @@ pub fn clone_repository(
 
     let _repo = repo_builder
         .clone(&repo_url, &repo_path.join(".bare"))
-        .expect("Could not clone repository...");
+        .expect("❌ Could not clone repository");
 
     let mut file = match File::create(repo_path.join(".git")) {
         Ok(file) => file,
@@ -112,7 +112,7 @@ pub fn clone_repository(
     };
 
     file.write_all(b"gitdir: ./.bare\n")
-        .expect("Could not write to the .git file.");
+        .expect("❌ Could not write to the .git file.");
     println!(
         "✅ Repository '{}' cloned successfully at '{}'",
         repo_url,
